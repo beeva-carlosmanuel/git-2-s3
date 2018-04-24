a03ccb6662fa0aacf5e7ce6d8bd7f5166bdc2607
Carlos Díaz <carlos.manuel@beeva.com>
2018-04-24 17:43:46 +0200

    push

--- loop.sh
+++ loop.sh
@@ -2,15 +2,11 @@ clear
 line=1
 git diff --name-only origin/develop..HEAD
 files2Push="$(git diff --name-only origin/develop..HEAD)"
-echo "Contenido: $files2Push"
+tab='\t'
+echo "Contenido: "
+echo $tab."$files2Push"
 
 while IFS= read -r line
 do
    echo "linea: $line"
 done < <(printf '%s\n' "$files2Push")
-
-
-# while IFS= read -r value; do
-#  echo "LINE $line : value $value"
-#  (( line ++ ))
-# done < files2Push
